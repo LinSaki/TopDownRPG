cba312669837b80da9d62f35a6c76dda618bdaa9
LinSaki <saquika@sheridancollege.ca>
2026-04-02 13:22:21 -0400

    Imported Idle and Walking animations

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,11 +8,13 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     [SerializeField]private float moveSpeed = 5f; //Serialize to edit in Unity Inspector
     private Vector2 moveInput;
+    private Animator animator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>(); //get the componenet of what this is attached to
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,6 +25,16 @@ public class PlayerMovement : MonoBehaviour
 
     public void Move(InputAction.CallbackContext context)
     {
-        moveInput = context.ReadValue<Vector2>();
+        animator.SetBool("isWalking", true);
+
+        if(context.canceled) //when movement is stopped on keyboard or analog stick
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+        }
+        moveInput = context.ReadValue<Vector2>(); //most recent context read value
+        animator.SetFloat("InputX", moveInput.x);
+        animator.SetFloat("InputY", moveInput.y);
     }
 }
