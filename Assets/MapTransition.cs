318a08f3ddce98f38831b31b40278fecf73e4487
LinSaki <saquika@sheridancollege.ca>
2026-04-09 15:18:49 -0400

    Updated MapTransition

--- Assets/MapTransition.cs
+++ Assets/MapTransition.cs
@@ -5,6 +5,10 @@ public class MapTransition : MonoBehaviour
 {
     [SerializeField] PolygonCollider2D mapBoundary;
     CinemachineConfiner2D confiner;
+    [SerializeField] Direction direction;
+    [SerializeField] float addPos = 1.75f;
+
+    enum Direction { Up, Down, Left, Right }
 
     private void Awake()
     {
@@ -16,8 +20,30 @@ public class MapTransition : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             confiner.BoundingShape2D = mapBoundary;
+            UpdatePlayerPosition(collision.gameObject);
         }
     }
 
+    private void UpdatePlayerPosition(GameObject player)
+    {
+        Vector3 newPos = player.transform.position;
 
+        switch (direction)
+        {
+            case Direction.Up:
+                newPos.y += addPos;
+                break;
+            case Direction.Down: 
+                newPos.y -= addPos;
+                break;
+            case Direction.Left:
+                newPos.x += addPos;
+                break;
+            case Direction.Right:
+                newPos.x -= addPos;
+                break;
+        }
+
+        player.transform.position = newPos;
+    }
 }
